2c134da278cc2ebf7cc839874dee674dc2601ae1
0i99 <0i99@pm.me>
2022-12-28 19:19:32 +0100

    gmap init

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import { BootstrapVue3 } from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+import VueGoogleMaps from "@fawmi/vue-google-maps";
+
+createApp(App)
+    .use(router)
+    .use(BootstrapVue3)
+    .use(VueGoogleMaps, {
+        load: {
+          key: ""
+        }
+      })
+    .mount('#app')
---
+++ webapp/src/views/SearchView.vue
@@ -0,0 +1,54 @@
+<template>
+<div >
+    <div class="container rounded">
+      <MarkerMap :markers="markers" />
+    </div>
+</div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import MarkerMap from '@/components/MarkerMap.vue'
+
+export default {
+  name: 'SearchView',
+  components: {
+    MarkerMap
+  },
+  data() {
+    return {
+      markers: [
+        {
+          position: {
+            lat: 50.08476746164984,
+            lng: 18.97534475266365,
+          },
+        },
+        {
+          position: {
+            lat: 50.08167771832807, 
+            lng: 18.985255631312956,
+          },
+        },
+        {
+          position: {
+            lat: 50.07090064441086, 
+            lng: 18.997151613185807,
+          },
+        },
+      ],
+    };
+  },
+}
+</script>
+
+<style scoped>
+  .container{
+    width: 50vw;
+    margin: auto;
+    background-color: red;
+    padding: 10px;
+    margin-top: 10px;
+  }
+</style>
